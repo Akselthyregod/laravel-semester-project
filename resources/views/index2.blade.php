5cfbba87fb086e6ee010edd7284dba54033f1819
Akselthyregod <59265356+Akselthyregod@users.noreply.github.com>
2021-11-17 11:12:50 +0100

    table added using bootstrap

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -5,12 +5,23 @@
 
     <h1>Testss</h1>
 
-    <ul>
-        @foreach($tests as $test)
-            <li>{{$test->id}}</li>
-        @endforeach
-    </ul>
-
-
+    <table class="table">
+        <thead>
+            <th scope="col">ID</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Updated at</th>
+            <th scope="col">Tests</th>
+        </thead>
+        <tbody>
+            @foreach($tests as $test)
+                <tr>
+                    <th scope="row">{{$test->id}}</th>
+                    <td>{{$test->created_at}}</td>
+                    <td>{{$test->updated_at}}</td>
+                    <td>{{$test->tests}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
