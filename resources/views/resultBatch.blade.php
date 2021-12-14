fe270caa7b844aa4605ee870a0cf994fddd1b4e6
Christian <bantex99@hotmail.com>
2021-12-14 12:28:22 +0100

    batch report

--- resources/views/resultBatch.blade.php
+++ resources/views/resultBatch.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,10 +6,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Batch report</title>
+    <link rel="stylesheet" href="{{URL::asset('css/styling.css')}}">
 </head>
 <body>
+@section('content')
 <h1>Batch report</h1>
 <a href="/batch">Back</a>
+
+<table class="table">
+    <thead>
+    <th scope="col">ID</th>
+    <th scope="col">Created at</th>
+    <th scope="col">Updated at</th>
+    <th scope="col">Processed</th>
+    <th scope="col">Defective</th>
+    <th scope="col">Speed</th>
+    <th scope="col">Humidity</th>
+    <th scope="col">Temperature</th>
+    <th scope="col">Vibration</th>
+    </thead>
+    <tbody>
+    @foreach($batch as $batch)
+        <tr>
+            <th scope="row">{{$batch->id}}</th>
+            <td>{{$batch->created_at}}</td>
+            <td>{{$batch->updated_at}}</td>
+            <td>{{$batch->prod_processed_count}}</td>
+            <td>{{$batch->prod_defective_count}}</td>
+            <td>{{$batch->mach_speed}}</td>
+            <td>{{$batch->humidity}}</td>
+            <td>{{$batch->temperature}}</td>
+            <td>{{$batch->vibration}}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 </body>
+@endsection
 </html>
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,62 @@ Route::post('/batch/create', [\App\Http\Controllers\BatchController::class, 'cre
 Route::get('/batch/result', [\App\Http\Controllers\BatchController::class, 'result']);
 
 Route::get('/ingredients', [\App\Http\Controllers\BatchController::class, 'ingredients']);
+
+/*
+
+Route::get('/db_dump', function () {
+
+    //Needed in SQL File:
+
+    //SET GLOBAL sql_mode = '';
+    //SET SESSION sql_mode = '';
+
+    $get_all_table_query = "SHOW TABLES";
+    $result = DB::select(DB::raw($get_all_table_query));
+
+    $tables = [
+        'live_batches',
+    ];
+
+    $structure = '';
+    $data = '';
+    foreach ($tables as $table) {
+        $show_table_query = "SHOW CREATE TABLE " . $table . "";
+
+        $show_table_result = DB::select(DB::raw($show_table_query));
+
+        foreach ($show_table_result as $show_table_row) {
+            $show_table_row = (array)$show_table_row;
+            $structure .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
+        }
+        $select_query = "SELECT * FROM " . $table;
+        $records = DB::select(DB::raw($select_query));
+
+        foreach ($records as $record) {
+            $record = (array)$record;
+            $table_column_array = array_keys($record);
+            foreach ($table_column_array as $key => $name) {
+                $table_column_array[$key] = '`' . $table_column_array[$key] . '`';
+            }
+
+            $table_value_array = array_values($record);
+            $data .= "\nINSERT INTO $table (";
+
+            $data .= "" . implode(", ", $table_column_array) . ") VALUES \n";
+
+            foreach($table_value_array as $key => $record_column)
+                $table_value_array[$key] = addslashes($record_column);
+
+            $data .= "('" . implode("','", $table_value_array) . "');\n";
+        }
+    }
+    $file_name = __DIR__ . '/../database/database_backup_on_' . date('y_m_d') . '.sql';
+    $file_handle = fopen($file_name, 'w + ');
+
+    $output = $structure . $data;
+    fwrite($file_handle, $output);
+    fclose($file_handle);
+    echo "DB backup ready";
+});
+
+*/
